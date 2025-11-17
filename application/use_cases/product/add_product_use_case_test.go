fad49b65aefb50af7f7cb6001ae2403f7501c62d
rinattynctykuulu <rinatala-too@mail.ru>
2025-11-17 17:41:27 +0600

    moq

--- application/use_cases/product/add_product_use_case_test.go
+++ application/use_cases/product/add_product_use_case_test.go
@@ -1,7 +1,41 @@
 package product
 
-import "testing"
+import (
+	"testing"
+	"waiter/domain/product/entities"
+	"waiter/domain/product/enums"
+)
 
 func Test_AddProduct_WhenIsSuccess(t *testing.T) {
+	product, _ := entities.NewProduct(
+		"cola",
+		enums.Thing,
+		"123456789",
+		"")
 
+	mock := &ProductRepositoryMock{
+		AddFunc: func(product entities.Product) (bool, error) {
+			return true, nil
+		},
+	}
+	isAdded, _ := mock.Add(*product)
+	if !isAdded {
+		t.Error("add product not saved")
+	}
+}
+
+func Test_UpdateProduct_WhenIsSuccess(t *testing.T) {
+
+}
+
+func Test_AddProduct_IsError(t *testing.T) {
+	_, errorMsg := entities.NewProduct(
+		"",
+		enums.Thing,
+		"",
+		"")
+
+	if errorMsg == nil {
+		t.Error(errorMsg)
+	}
 }
--- domain/product/repositories/product_repository.go
+++ domain/product/repositories/product_repository.go
@@ -1,1 +1,12 @@
 package repositories
+
+import "waiter/domain/product/entities"
+
+type ProductRepository interface {
+	Add(product entities.Product) (bool, error)
+	GetByCode(code string) (entities.Product, error)
+	Edit(product entities.Product) (bool, error)
+	GetByName(name string) (entities.Product, error)
+	GetAll() ([]entities.Product, error)
+	Get(uuid string) (entities.Product, error)
+}
